1489144217c900cc487ce885c4e0b1b6c1aece69
Chris McIntosh <rougechampion2002@gmail.com>
2015-12-30 09:23:19 -0600

    ctypes test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+from ctypes import *
+
 def printMe():
         print "stuff and things?"
 
+def useCTYPES():
+        msvcrt = cdll.msvcrt
+        message_string = "Hello world!\n"
+        msvcrt.printf("Testing: %s",message_string)
+
 if __name__ == "__main__":
         printMe()
+        useCTYPES()
