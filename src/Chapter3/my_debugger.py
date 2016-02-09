988fbb46193d4cd1aa9cac2d49c02c12df476a8a
Chris McIntosh <rougechampion2002@gmail.com>
2016-02-08 22:37:03 -0600

    Mem Breakpoint code

--- src/Chapter3/my_debugger.py
+++ src/Chapter3/my_debugger.py
@@ -13,6 +13,15 @@ class debugger():
             self.breakpoints = {}
             self.first_breakpoints = True
             self.hardware_breakpoints = {}
+            self.guarded_pages = []
+            self.memory_breakpoints = {}
+
+            # Here lets determine and store the default page size for the system
+            system_info = SYSTEM_INFO()
+            kernel32.GetSystemInfo(byref(system_info))
+
+            self.page_size = system_info.dwPageSize
+
 
 
         def load(self, path_to_exe):
@@ -311,7 +320,31 @@ class debugger():
 
             return True
 
+        def bp_set_mem(self, address, size):
+            mbi = MEMORY_BASIC_INFORMATION()
 
+            # If our VirtualQueryEx() call doesn't return a full-sized MEMORY_BASIC_INFORMATION then return False
+            if kernel32.VirtualQueryEx(self.h_process, address, byref(mbi), sizeof(mbi)) < sizeof(mbi):
+                return False
+
+            current_page = mbi.BaseAddress
+
+            # We will set the permsissions on all pages that are affected by our memory breakpoint
+            while current_page <= address + size:
+                # add the page to the listl this will differentiate our guarded pages from those that were set by the os or the debuggee process
+                self.guarded_pages.append(current_page)
+
+                old_protection = c_ulong(0)
+                if not kernel32.VirtualProtectEx(self.h_process, current_page, size, mbi.Protect | PAGE_GUARD,
+                                                 byref(old_protection)):
+                    return False
+
+                # Increates our range by the size of the default system meormy page_size
+                current_page += self.page_size
+
+            self.memory_breakpoints[address] = (address, size, mbi)
+
+            return True
 
         def func_resolve(self, dll, function):
             handle = kernel32.GetModuleHandleA(dll)
