716d4bcb6cbb0cc9795d7e7cada699ea22375914
Chris McIntosh <rougechampion2002@gmail.com>
2016-02-08 21:04:50 -0600

    next set

--- src/Chapter3/my_debugger.py
+++ src/Chapter3/my_debugger.py
@@ -82,7 +82,28 @@ class debugger():
 
                 print "Event Code: %d Thread ID: %d" % (debug_event.dwDebugEventCode, debug_event.dwThreadId)
 
+                # If the event is an exception we want to exampine it further
+                if debug_event.dwDebugtEventCode == EXCEPTION_DEBUG_EVENT:
+                    # Obtain the exception code
+                    exception = debug_event.u.Exception.ExceptionRecord.ExceptionCode
+                    self.exception_address = debug_event.u.Exception.ExceptionRecord.ExceptionAddress
+
+                if exception == EXCEPTION_ACCESS_VIOLATION:
+                    print "Access violation detected."
+                # If a breakpoint is detected, we call an internal handler
+                elif exception == EXCEPTION_BREAKPOINT:
+                    continue_status = self.exception_handler_breakpoint()
+                elif exception == EXCEPTION_GUARD_PAGE:
+                    print "Guard page access detected."
+                elif exception == EXCEPTION_SINGLE_STEP:
+                    print "Single step exception detected"
+
                 kernel32.ContinueDebugEvent(debug_event.dwProcessId, debug_event.dwThreadId, continue_status)
+
+            def exception_handler_breakpoint():
+                print "[*] Inside breakpoint handler."
+                print "Exception Address: 0x%08x" % self.exception_address
+                return DBG_CONTINUE
 
         def detach(self):
             if kernel32.DebugActiveProcessStop(self.pid):
