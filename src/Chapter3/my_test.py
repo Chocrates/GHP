5d2c25863fdc77e097c6a98391968f36ef5be5bc
Chris McIntosh <rougechampion2002@gmail.com>
2016-02-08 22:23:28 -0600

    HW Breakpoint code

--- src/Chapter3/my_test.py
+++ src/Chapter3/my_test.py
@@ -1,5 +1,5 @@
 import my_debugger
-
+from my_debugger_defines import *
 debugger = my_debugger.debugger()
 
 pid = raw_input("Enter the PID of the process to attach to: "
@@ -7,7 +7,7 @@ pid = raw_input("Enter the PID of the process to attach to: "
 debugger.attach(int(pid))
 printf_address = debugger.func_resolve("msvcrt.dll", "printf")
 print "[*] Address of printf: 0x%08x" % printf_address
-debugger.bp_set_hw(printf_address, 1, debugger.HW_EXECUTE)
+debugger.bp_set_hw(printf_address, 1, HW_EXECUTE)
 
 debugger.run()
 
