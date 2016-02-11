c4ea5a962d33095d260b001e7896ddc106252b09
Chris McIntosh <rougechampion2002@gmail.com>
2016-02-10 22:08:32 -0600

    forgot to turn it in to a char pointer so i was trying to assign a string to a character type

--- src/Chapter4/buffer_overflow.py
+++ src/Chapter4/buffer_overflow.py
@@ -5,7 +5,7 @@ msvcrt = cdll.msvcrt
 # Give the debugger time to attache then hit a button
 raw_input("Once the debugger is attached, press any key.")
 
-buffer = c_char("AAAAA")
+buffer = c_char_p("AAAAA")
 overflow = "A" * 100
 
 msvcrt.strcpy(buffer, overflow)
