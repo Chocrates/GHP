9d0730d8d4e5b51c217790e97947abf256e27d98
Chris McIntosh <rougechampion2002@gmail.com>
2016-02-10 22:07:12 -0600

    access violation handler

---
+++ src/Chapter4/buffer_overflow.py
@@ -0,0 +1,11 @@
+from ctypes import *
+
+msvcrt = cdll.msvcrt
+
+# Give the debugger time to attache then hit a button
+raw_input("Once the debugger is attached, press any key.")
+
+buffer = c_char("AAAAA")
+overflow = "A" * 100
+
+msvcrt.strcpy(buffer, overflow)
